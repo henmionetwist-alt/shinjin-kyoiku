ce0d999e36824e851a2b1f7bffe0d7bb86044fff
henmionetwist-alt <henmi.onetwist@gmail.com>
2026-09-13 22:23:01 +0900

    chore: アプリをリポジトリ直下に統合

---
+++ js/firebase-config.js
@@ -0,0 +1,14 @@
+/* =========================================================
+   Firebase 設定
+   ========================================================= */
+const firebaseConfig = {
+  apiKey: "AIzaSyCdN9MOW83F3DlmL78Cr3kZSEBS-8MPz-4",
+  authDomain: "shinjin-kyoiku.firebaseapp.com",
+  projectId: "shinjin-kyoiku",
+  storageBucket: "shinjin-kyoiku.firebasestorage.app",
+  messagingSenderId: "545027998453",
+  appId: "1:545027998453:web:249e05272878edabc53bbc"
+};
+
+/* アプリ名（画面上部とホーム画面のアイコン下に表示） */
+const APP_NAME = "新人教育";
